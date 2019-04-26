6464ed5bf96c7483986fef7df697a08c9cfb59b9
Petar <parandjic@gmail.com>
2019-04-26 23:47:18 +0200

    Workig example of autoComplete

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# autocomplete
-I'll make my own autocomplete with blackjack and absolute input field
+# autocomplete for absolute position input field;
+I'll make my own autocomplete with blackjack and absolute input field;
---
+++ demo/js/auto_complete_parametars.js
@@ -0,0 +1,14 @@
+//Here you setup parametars for autocomplete
+	
+	//save you arrays of data to this array to use in your autocomplete
+	var ser_val = new Array();
+		//example
+		ser_val[0] = ['USA', 'Germany', 'China', 'Rusia', 'Mexico','Japan','Japan1','Japan2','Japan3','Japan4','Japan5','Japan6','japan7']
+		ser_val[1] = ['Audi', 'Golf', 'Skoda', 'BMW', 'Jaguar', 'Honda'];
+
+	//set how many result do you want input to show
+	var set_ac_length = 6;
+
+	//set height of autocomplete window
+	var set_ac_height = 100;
+
